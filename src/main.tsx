fa456ab63b4a618d938ab311269cf07d32f0acb1
gamppe <felix0703@naver.com>
2025-10-10 01:04:41 +0900

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -8,14 +8,6 @@ import ResetStyle from "./ResetStyle.tsx";
 import { RouterProvider } from "react-router-dom";
 import router from "./Router.tsx";
 
-const Container = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 100vh;
-  width: 100vw;
-  background-color: #505050;
-`;
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ResetStyle />
