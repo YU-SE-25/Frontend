3716eb31dcd31a02b8a484724b322b4f9e896803
HyeongseopKim <49994494+gamppe@users.noreply.github.com>
2025-10-10 01:26:13 +0900

    스크린 폴더 생성, browserrouter방식에서 createbrowserrouter방식으로 변경 (#7)
    * [수정] toolbar 보이게
    
    [수정] toolbar 보이게
    
    * [추가 및 수정]
    
    수정 : Topbar 컴포넌트 레이아웃
    추가 : 초기 페이지, 기본 라우팅
    
    * [문서] README.md 커밋 메세지 작성 규칙 추가
    
    * 폴더 위치 변경
    
     screens폴더로 변경
    
    * create browser router로 수정
    
    browser router -> createbrowserroter 방식으로 수정...
    
    * Update main.tsx
    
    ---------
    
    Co-authored-by: 전유진 <123734118+zzzin18@users.noreply.github.com>
    Co-authored-by: Woohyeon Roh <ykjrco@naver.com>

--- src/main.tsx
+++ src/main.tsx
@@ -4,28 +4,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import styled from "styled-components";
-import App from "./App.tsx";
 import ResetStyle from "./ResetStyle.tsx";
-import Topbar from "./components/Topbar.tsx";
-import { BrowserRouter } from "react-router-dom";
+import { RouterProvider } from "react-router-dom";
+import router from "./Router.tsx";
 
-const Container = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 100vh;
-  width: 100vw;
-  background-color: #505050;
-`;
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ResetStyle />
-
-    <BrowserRouter>
-      <Container>
-        <Topbar />
-        <App />
-      </Container>
-    </BrowserRouter>
+    <RouterProvider router={router} />
   </StrictMode>
 );
---
+++ src/screens/Register.tsx
@@ -0,0 +1,6 @@
+//import React from "react";
+//import styled from "styled-components";
+
+export default function Register() {
+  return <h1>회원가입</h1>;
+}
