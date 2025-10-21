795dd4477480c1275b980f5f64d6862de89f8dac
gamppe <felix0703@naver.com>
2025-10-21 18:21:31 +0900

    import axios
    axios와 @types/axios설치

--- src/components/Topbar.tsx
+++ src/components/Topbar.tsx
@@ -1,6 +1,6 @@
 import { Link, NavLink, useNavigate } from "react-router-dom";
 import styled from "styled-components";
-//import axios from "axios"; -> 전역관리용 추후 결정예정
+import axios from "axios"; //-> 전역관리용 추후 결정예정
 
 //전역 상태 관리 (Zustand/Jotai를 위한 Placeholder)
 //실제 프로젝트에서는 이 부분을 useAuthStore.ts 등에서 import 해야 합니다.
@@ -171,7 +171,7 @@ export default function Topbar({ isLoggedIn = false }: TopbarProps) {
               <AuthLink to="/login">로그인</AuthLink>
               <AuthLink to="/register">회원가입</AuthLink>
               {/*마이페이지 및 로그아웃 버튼 위치 추후 수정 예정*/}
-              <AuthLink to="/mypage/:userName">삭제예정 - 마이페이지</AuthLink>
+              <AuthLink to="/mypage/:userName">마이페이지</AuthLink>
               <AuthLink to="/mypage">로그아웃</AuthLink>
             </>
           )}
