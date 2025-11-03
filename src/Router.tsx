367464282988ccfc190a4f30045ba39a3d12388e
전유진 <123734118+zzzin18@users.noreply.github.com>
2025-11-04 01:54:04 +0900

    [추가 및 수정] 문제 추가 추가,  css 수정
    - 문제 추가 기능 및 css 추가(추후 api관련 수정 필요)
    - 문제 목록 중 기록 기능 수정(로그인 한 회원이 푼 문제 관련 필터링 기능, css 또한 수정)
    -문제 목록 및 문제 상세 화면 속 글자 크기 및 배치 css 수정

--- src/Router.tsx
+++ src/Router.tsx
@@ -13,9 +13,12 @@ import Mypage from "./screens/Mypage";
 
 import ProblemList from "./problem/ProblemList";
 import ProblemDetail from "./problem/ProblemDetail";
+import ProblemAdd from "./problem/ProblemAdd";
 
 import Board from "./screens/Board";
+
 import Studygroup from "./screens/Studygroup";
+
 import NotFound from "./screens/NotFound";
 
 const router = createBrowserRouter([
@@ -26,8 +29,10 @@ const router = createBrowserRouter([
       { index: true, element: <Home /> },
       { path: "problem-list", element: <ProblemList /> },
       { path: "problem-detail/:problemId", element: <ProblemDetail /> },
+      { path: "problem-add", element: <ProblemAdd /> },
 
       { path: "board", element: <Board /> },
+
       { path: "studygroup", element: <Studygroup /> },
 
       { path: "login", element: <Login /> },
