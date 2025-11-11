7ec6f27321748edd4deb57e72247630d499ab74f
gamppe <felix0703@naver.com>
2025-11-11 20:28:12 +0900

    Revert "add managepage"
    This reverts commit 8246f8dc9dd67374377bb9562820026568609845.

--- src/api/dummy/mypage_dummy.ts
+++ src/api/dummy/mypage_dummy.ts
@@ -236,5 +236,5 @@ export async function getRecentSubmissions(
 */
 //mamagerUser, instructorUser, dummyUser 중에서 역할에 따라 반환
 export async function getDummyUserProfile(): Promise<UserProfile> {
-  return Promise.resolve(instructorUser);
+  return Promise.resolve(managerUser);
 }
