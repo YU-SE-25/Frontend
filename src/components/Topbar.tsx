768ba1ed888d404bb66101c54d1d4fcae45f875a
gamppe <felix0703@naver.com>
2025-11-20 19:39:16 +0900

    Update Topbar.tsx

--- src/components/Topbar.tsx
+++ src/components/Topbar.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { Link, NavLink, useNavigate } from "react-router-dom";
 import styled, { keyframes, css } from "styled-components";
 import axios from "axios";
