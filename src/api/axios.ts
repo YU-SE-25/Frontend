b9a92dc575d7f13b12d6f97a2a7b4d694e79ee2c
HyeongseopKim <49994494+gamppe@users.noreply.github.com>
2025-11-29 21:52:27 +0900

    [추가] 강사가 제출한 문제내역 추가, Qna 수정버튼 추가 (#79)
    [수정]신고 버튼 위치 옮기기
    
    * [추가] 게시판과 공유된풀이에 수정/삭제버튼 추가
    
    수정과 삭제버튼 추가했습니다. api 연동필요!
    
    * [추가] 리뷰 좋아요 버튼
    
    좋아요 버튼 추가!
    
    * Update qna_dummy.ts
    
    * [삭제]@types/axios 삭제
    
    axios의 타입선언이 문제를 일으킬 수 있다고 해서 삭제했습니다.
    
    * [수정] QNA 수정버튼 추가, 디테일 형식 IProblem에 맞게 수정
    
    * [추가] 강사가 제출한 문제 내역 추가
    
    * [수정] 마이페이지 get api 수정
    
    마이페이지 get api 수정, 하지만 아직 작동 안됨
    
    * Add debug logs and update test username in MyPageLayout
    
    Added a console.log for userProfile in Topbar for debugging purposes. Updated the test username in MyPageLayout from 'gamppe' to '김형섭' for getUserProfile.

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -4,3 +4,15 @@ export const api = axios.create({
   baseURL: "/api",
   withCredentials: true,
 });
+
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("accessToken");
+    if (token) {
+      config.headers = config.headers ?? {};
+      config.headers["Authorization"] = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
