93ee2cb66dbd2858d583ce67ddfa21d71c9eb32c
전유진 <123734118+zzzin18@users.noreply.github.com>
2025-12-03 10:24:06 +0900

    [수정] 문제 api 수정
    - 문제쪽 api 수정

--- src/api/login_api.ts
+++ src/api/login_api.ts
@@ -6,55 +6,13 @@ interface LoginPayload {
   password: string;
   keepLogin: boolean;
 }
-/*
-//MOCK API
-export async function postLogin(data: LoginPayload): Promise<LoginResponse> {
-  console.log(`[MOCK API] Post Login Called with: ${JSON.stringify(data)}`);
-
-  // 2초 딜레이 시뮬레이션
-  await new Promise((resolve) => setTimeout(resolve, 2000));
-
-  // 실패 조건
-  if (data.password === "fail") {
-    const error = new Error("사용자 정보가 일치하지 않습니다.");
-    (error as any).response = {
-      status: 401,
-      data: { message: "사용자 정보가 일치하지 않습니다." },
-    };
-    throw error;
-  }
-
-  // 성공 응답 Mock
-
-  // debugging용
-  // return {
-  //   accessToken: "mock_access_token_123",
-  //   refreshToken: "mock_refresh_token_xyz",
-  //   expiresIn: 3600,
-  //   user: { userId: 1, nickname: "gamppe", role: "LEARNER" },
-  // };
-
-  return {
-    accessToken: "mock_access_token_123",
-    refreshToken: "mock_refresh_token_xyz",
-    expiresIn: 3600,
-    user: { userId: 1, nickname: "gamppe", role: "INSTRUCTOR" },
-  };
-
-  //  return {
-  //   accessToken: "mock_access_token_123",
-  //   refreshToken: "mock_refresh_token_xyz",
-  //   expiresIn: 3600,
-  //   user: { userId: 1, nickname: "gamppe", role: "MANAGER" },
-  // };
-}
-*/
-
-// ===== 실제 API 통신 (백엔드 준비 후 활성화) =====
 
 export async function postLogin(data: LoginPayload): Promise<LoginResponse> {
   const response = await api.post<LoginResponse>("auth/login", data);
-  //응답값 확인용
-  console.log("login response:", response);
-  return response.data;
+  const loginData = response.data;
+
+  localStorage.setItem("accessToken", loginData.accessToken);
+  localStorage.setItem("refreshToken", loginData.refreshToken);
+
+  return loginData;
 }
